986074010d56117eb70b11037f2514b37fd9dccc
Rokan Faruqui <rufaruqui@gmail.com>
2018-11-03 18:56:20 +0600

    role string to small int

--- db/migrate/20181102083354_change_roles_from_string_to_integer_in_workforces.rb
+++ db/migrate/20181102083354_change_roles_from_string_to_integer_in_workforces.rb
@@ -1,5 +1,6 @@
 class ChangeRolesFromStringToIntegerInWorkforces < ActiveRecord::Migration[5.1]
   def change
-     change_column :workforces, :role, type:integer{2}, limit:1, default:0
+     remove_column :workforces, :role
+     add_column :workforces, :role, :integer, limit:1, default:0
   end
 end
